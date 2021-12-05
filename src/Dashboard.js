4ba4dc7bcaf6365b8c4b4c69f4fb24d197ca3be4
Balgow <dikhanbalgynbek01@gmail.com>
2021-12-05 21:52:10 +0600

    creating dashboard

---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Dashboard() {
+    return (
+      <div className="Dashboard">
+          <h1>Hi</h1>
+          <GlobalStyle />
+      </div>
+    );
+  }
+  
+  export default App;
+  
