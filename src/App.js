71c2ba8700eb3a2b485699bf65784753792c1636
Emir Kuanyshev <emir.kuanyshev@nu.edu.kz>
2021-12-06 17:44:29 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import React from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import Search from './pages/Search';
 import Dashboard from './pages/Dashboard';
-
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard></Dashboard>
-    </div>
+      <Router>
+          <ChakraProvider>
+            <div className="App">
+                {/*компонент HEADER сюда вставить*/}
+                <Switch>
+                    <Route path={"/dashboard"}>
+                        <Dashboard/>
+                    </Route>
+                    <Route path={"/"}>
+                        <Redirect
+                            to={{
+                                pathname: "/dashboard",
+                            }}
+                        />
+                    </Route>
+                </Switch>
+            </div>
+          </ChakraProvider>
+      </Router>
   );
 }
 
