761682a9af87eb82973fdcbbac6ec71b59543721
Emir Kuanyshev <emir.kuanyshev@nu.edu.kz>
2021-12-06 21:47:18 +0600

    room list added

--- src/pages/rooms-list.jsx
+++ src/pages/rooms-list.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import {Box, Text} from "@chakra-ui/react";
-
+import {Box, Button, Input, Text} from "@chakra-ui/react";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure
+} from '@chakra-ui/react'
+import { Select } from '@chakra-ui/react'
 
 const testData = [
     {
@@ -39,23 +49,143 @@ const testData = [
 
 export const RoomsList = () => {
 
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+
     return (
         <Box
             backgroundColor={"#dfe6e9"}
-            height={"100%"}
+            height={"calc(100vh - 60px)"}
             width={"100%"}
             display={"flex"}
             flexDirection={"column"}
             alignItems={"center"}
         >
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Booking</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Box
+                            marginBottom={"10px"}
+                        >
+                            <Text>From</Text>
+                            <Input placeholder="select date"/>
+                        </Box>
+                        <Box>
+                            <Text>To</Text>
+                            <Input placeholder="select date"/>
+                        </Box>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Book</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
             <Box
                 marginTop={"34px"}
                 height={"70px"}
                 width={"calc(100% - 25px)"}
                 backgroundColor={"#b2bec3"}
                 borderRadius={"10px"}
+                display={"flex"}
+                flexDirection={"row"}
+                alignItems={"center"}
             >
-
+                <Box
+                    w={"100%"}
+                    display={"flex"}
+                    flexDirection={"row"}
+                    paddingLeft={"10px"}
+                    paddingRight={"10px"}
+                    paddingTop={"5px"}
+                    paddingBottom={"5px"}
+                    marginBottom={"10px"}
+                    borderRadius={"7px"}
+                    alignItems={"center"}
+                >
+                    <Box
+                        flex={"1 1 0px"}
+                        display={"flex"}
+                        justifyContent={"center"}
+                    >
+                        <Input
+                            placeholder={"filter by id"}
+                            width={"90%"}
+                            color={"black"}
+                            borderColor={"black"}
+                        />
+                    </Box>
+                    <Box
+                        flex={"1 1 0px"}
+                        display={"flex"}
+                        justifyContent={"center"}
+                    >
+                        <Input
+                            placeholder={"filter by floor"}
+                            width={"90%"}
+                            color={"black"}
+                            borderColor={"black"}
+                        />
+                    </Box>
+                    <Box
+                        flex={"1 1 0px"}
+                        display={"flex"}
+                        justifyContent={"center"}
+                    >
+                        <Input
+                            placeholder={"filter by room #"}
+                            width={"90%"}
+                            color={"black"}
+                            borderColor={"black"}
+                        />
+                    </Box>
+                    <Box
+                        flex={"1 1 0px"}
+                        display={"flex"}
+                        justifyContent={"center"}
+                    >
+                        <Input
+                            placeholder={"filter b"}
+                            width={"90%"}
+                            color={"black"}
+                            borderColor={"black"}
+                        />
+                    </Box>
+                    <Box
+                        flex={"1 1 0px"}
+                        display={"flex"}
+                        justifyContent={"center"}
+                    >
+                        <Select
+                            placeholder='Filter by status'
+                            width={"90%"}
+                        >
+                            <option value='ALL'>all</option>
+                            <option value='CLEANED'>cleaned</option>
+                            <option value='NOT_CLEANED'>not cleaned</option>
+                        </Select>
+                    </Box>
+                    <Box
+                        flex={"1 1 0px"}
+                        display={"flex"}
+                        justifyContent={"center"}
+                    >
+                        <Select
+                            placeholder='Filter by occupancy'
+                            width={"90%"}
+                        >
+                            <option value='ALL'>all</option>
+                            <option value='OCCUPIED'>occupied</option>
+                            <option value='FREE'>free</option>
+                        </Select>
+                    </Box>
+                </Box>
             </Box>
             <Box
                 marginTop={"14px"}
@@ -69,6 +199,7 @@ export const RoomsList = () => {
                     paddingRight={"10px"}
                     paddingTop={"5px"}
                     paddingBottom={"5px"}
+                    marginBottom={"10px"}
                     backgroundColor={"#636E72"}
                     borderRadius={"7px"}
                 >
@@ -76,7 +207,7 @@ export const RoomsList = () => {
                         flex={"1 1 0px"}
                     >
                         <Text
-                            color={"#336e7b"}
+                            color={"black"}
                         >
                             Room id
                         </Text>
@@ -85,29 +216,36 @@ export const RoomsList = () => {
                         flex={"1 1 0px"}
                     >
                         <Text
-                            color={"#336e7b"}
+                            color={"black"}
                         >Floor</Text>
                     </Box>
                     <Box
                         flex={"1 1 0px"}
                     >
                         <Text
-                            color={"#336e7b"}
+                            color={"black"}
                         >Number</Text>
                     </Box>
                     <Box
                         flex={"1 1 0px"}
                     >
                         <Text
-                            color={"#336e7b"}
+                            color={"black"}
                         >Status</Text>
                     </Box>
                     <Box
                         flex={"1 1 0px"}
                     >
                         <Text
-                            color={"#336e7b"}
+                            color={"black"}
                         >Occupancy</Text>
+                    </Box>
+                    <Box
+                        flex={"1 1 0px"}
+                    >
+                        <Text
+                            color={"black"}
+                        >Book</Text>
                     </Box>
                 </Box>
                 {
@@ -119,6 +257,7 @@ export const RoomsList = () => {
                                 borderRadius={"7px"}
                                 paddingLeft={"10px"}
                                 paddingRight={"10px"}
+                                marginBottom={"5px"}
                             >
                                 <Box
                                     flex={"1 1 0px"}
@@ -154,6 +293,20 @@ export const RoomsList = () => {
                                     <Text
                                         color={"#336e7b"}
                                     >{room.occupancy ? "Occupied" : "Free"}</Text>
+                                </Box>
+                                <Box
+                                    flex={"1 1 0px"}
+                                >
+                                    <Button
+                                        backgroundColor={"green"}
+                                        height={"32px"}
+                                        width={"100%"}
+                                        onClick={() => {
+                                            onOpen();
+                                        }}
+                                    >
+                                        Book
+                                    </Button>
                                 </Box>
                             </Box>
                         )
